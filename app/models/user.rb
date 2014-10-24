3e2a668eaf79fb4afe937fa1ccad55da480c6338
Nick Tho <matenaar92@gmail.com>
2014-10-24 13:01:52 +1100

    add new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,10 @@
 class User < ActiveRecord::Base
 	belongs_to :room
 	has_many :messages
+
+	has_secure_password
+
+	validates :name, presence: true, uniqueness: true, length: {minimum: 3}
+	validates :email, presence: true, uniqueness: true
+	validates_length_of :password, in: 3..20, on: :create
 end
