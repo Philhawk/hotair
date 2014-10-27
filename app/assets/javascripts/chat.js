e2d75c77a964d77cc95c20b21a8f010b5239203d
JamesLeBoeuf <jamesleboeuf@gmail.com>
2014-10-27 15:43:32 +1100

    Minor change to search feature

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -267,6 +267,8 @@ var joinRoom = function (room_id) {
 
 		room.unbind('scroll_chat');
 
+		room.unbind('new_search');
+
 
 		dispatcher.unbind('new_embed');
 		dispatcher.unbind('new_text');
@@ -275,6 +277,7 @@ var joinRoom = function (room_id) {
 		dispatcher.unbind('new_map');
 		dispatcher.unbind('new_recipe');
 		dispatcher.unbind('new_movie');
+		dispatcher.unbind('new_search');
 
 		// ADD BETWEEN HERE
 		// AND HERE
