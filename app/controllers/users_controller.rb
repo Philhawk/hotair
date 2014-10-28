1e16a66ec6121e923008682b4fba97ab12c846a9
lawrencely <lawrence.ly667@gmail.com>
2014-10-28 15:31:33 +1100

    new user redirect to chat path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 	def new
 		@user = User.new
-		redirect_to chat
+		redirect_to chat_path
 	end
 
 	def create
 		@user = User.new user_params
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to chat
+			redirect_to chat_path
 		else
 			render :new
 		end
