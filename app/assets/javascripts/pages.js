b1493161adc98d56a16ae6d9c515816eb4bff293
JamesLeBoeuf <jamesleboeuf@gmail.com>
2014-10-31 10:21:26 +1100

    bug fixes

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,7 +1,7 @@
-	$(document).ready(function() {
+$(document).ready(function() {
 	$('body').on('click', '#new_login_path', showLogin);
 	$('body').on('click', '#new_user_path', showRegister);
-  $('body').on('click', '#registerButton', showRegister);
+  $('body').on('click', '#register_button', showRegister);
 });
 
 var showLogin = function () {
@@ -13,7 +13,6 @@ var showRegister = function () {
 	// reveal the modal that contains the new room form
 	$('#RegisterModal').foundation('reveal', 'open');
 };
-
 
 var closeLoginPage = function () {	
 	$('#newLoginModal').foundation('reveal', 'close');
